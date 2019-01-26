2e6ce6be5d5b25a91316b85321738897ac52cb81
Rachel Haub <rachelhaub@gmail.com>
2019-01-26 13:20:40 -0800

    Added camera movement
    Third person camera movement dependent on mouse (y axis) and player position (x axis).

---
+++ CrabGamePart2/Assets/Scripts/CameraTestingScripts/CameraFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+    public GameObject target;
+    public float rotateSpeed = 5;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        //float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+        target.transform.Rotate(-vertical, 0, 0);
+
+        float desiredAngle_X = target.transform.eulerAngles.x;
+        float desiredAngle_Y = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(desiredAngle_X, desiredAngle_Y, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
---
+++ CrabGamePart2/Assets/Scripts/CameraTestingScripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0)));
+        rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed));
+
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+
+        rb.angularVelocity = new Vector3(rb.angularVelocity.x, 0, 0);
+    }
+}
