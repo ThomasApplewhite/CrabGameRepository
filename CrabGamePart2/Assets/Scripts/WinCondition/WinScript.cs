851c84b416e11812a140dd4eab2e896d84ff413e
Rachel Haub <rachelhaub@gmail.com>
2019-01-26 14:11:02 -0800

    Win Condition and Paricle effects

---
+++ CrabGamePart2/Assets/Scripts/WinCondition/WinScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Crab")
+        {
+            transform.SetParent(collision.gameObject.transform);
+            //go to cutscene
+        }
+    }
+}
