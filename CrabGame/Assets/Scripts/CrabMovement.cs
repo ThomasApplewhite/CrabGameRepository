aba162607f9d35370608f69067e6e9097409fe35
Thomas Applewhite <thomashapplewhite@gmail.com>
2019-01-25 20:45:32 -0800

    Added some objects for testing.
    Still working on shell behavior. Doesn't currently work properly, don't touch 'em.

---
+++ CrabGame/Assets/Scripts/CrabMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabMovement : MonoBehaviour {
+
+    public float MoveSpeed;
+    public Rigidbody physics;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	//x-coord is side-to-side, y-cord is up-down, z-cord is forward-back. Sorta
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            physics.AddForce(new Vector3(0, 0, MoveSpeed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            physics.AddForce(new Vector3(-MoveSpeed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            physics.AddForce(new Vector3(0, 0, -MoveSpeed));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            physics.AddForce(new Vector3(MoveSpeed, 0, 0));
+        }
+    }
+}
---
+++ CrabGame/Assets/Scripts/CrabProperties.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabProperties : MonoBehaviour {
+
+    public bool ShellOn;
+    public GameObject Shell;
+	// Use this for initialization
+	void Start () {
+        ShellOn = false;
+	}
+
+    void OnCollisionEnter(Collision shell)
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Shell = shell.gameObject;
+            ShellOn = true;
+            //something that changes the other thing
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (ShellOn)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Shell.Get.ComponentTransform.Translate(transfrom.position.x + 5, transform.position.y + 5, transform.position.z + 5);
+                //Shell.ShellPower();
+
+            }
+
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                //Doesn't work atm
+                Debug.Log("Snip");
+
+            }
+           
+        }
+        
+	}
+
+    
+}
