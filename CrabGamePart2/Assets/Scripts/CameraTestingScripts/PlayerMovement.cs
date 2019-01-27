3e919097f790bd1dc53d1596905913480e877366
Thomas Applewhite <thomashapplewhite@gmail.com>
2019-01-27 14:39:03 -0800

    Started sound implementation

--- CrabGamePart2/Assets/Scripts/CameraTestingScripts/PlayerMovement.cs
+++ CrabGamePart2/Assets/Scripts/CameraTestingScripts/PlayerMovement.cs
@@ -9,17 +9,23 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
+    AudioSource walkSound;
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        walkSound = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
     {
         rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0)));
         rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed));
+        if((Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) && !walkSound.isPlaying)
+        {
+            walkSound.Play();
+        }
 
         if (Input.GetKeyDown("space"))
         {
--- CrabGamePart2/Assets/Scripts/CrabMovement.cs
+++ CrabGamePart2/Assets/Scripts/CrabMovement.cs
@@ -6,28 +6,34 @@ public class CrabMovement : MonoBehaviour {
 
     public float MoveSpeed = 10.0f;
     public Rigidbody physics;
+    public AudioSource walkSound;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //walkSound = GetComponent<AudioSource>();
+        walkSound.Play();
+    }
 	//x-coord is side-to-side, y-cord is up-down, z-cord is forward-back. Sorta
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.W))
         {
             physics.AddForce(new Vector3(0, 0, MoveSpeed));
+            walkSound.Play(); //loop = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
             physics.AddForce(new Vector3(-MoveSpeed, 0, 0));
+            walkSound.Play(); //loop = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
             physics.AddForce(new Vector3(0, 0, -MoveSpeed));
+            walkSound.Play(); //loop = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
             physics.AddForce(new Vector3(MoveSpeed, 0, 0));
+            walkSound.Play(); //loop = true;
         }
     }
 }
